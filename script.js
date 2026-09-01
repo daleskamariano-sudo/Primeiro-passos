81660dbc3b7684cc210497996a75fb235bf852ee
daleskamariano-sudo <daleska.mariano@escola.pr.gov.br>
2026-09-01 10:57:07 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector("button");
+botoes.forEach(function(botao) {
+    let curtiu = false;
+botao.addEventListener("click", botaoClicado);
+function botaocjicado () { 
+     consolete.log ( "fui clicado");
+     let texto = botao.querySelector("span");
+     if (curtiu === false) {
+        
+     }
+     texto.textoContent++;
+}
+});
